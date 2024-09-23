d69a0b1aab2a53cf36f993407301a556d2e04278
Noahnox <noah.whiddoncarlsson@gmail.com>
2024-09-23 15:14:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import ifcopenshell
 from .rules import windowRule
 from .rules import doorRule
 
-model = ifcopenshell.open("C:\Users\noahw\OneDrive\Skrivbord\ifc file")
+model = ifcopenshell.open("C:\Users\noahw\OneDrive\Skrivbord\model/")
 
 windowResult = windowRule.checkRule(model)
 doorResult = doorRule.checkRule(model)
