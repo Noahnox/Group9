ad1e850c96e11a5c794b973be908a13f09e25929
Noahnox <noah.whiddoncarlsson@gmail.com>
2024-09-23 15:06:53 +0200

    Update wallRule.py

--- rules/wallRule.py
+++ rules/wallRule.py
@@ -1,5 +1,5 @@
 import ifcopenshell
-model = ifcopenshell.open('/path/to/your/model.ifc')
+model = ifcopenshell.open('C:\Users\noahw\OneDrive\Skrivbord\model')
 
 walls = model.by_type('IfcWall')
 print(f"Walls in model: {len(walls)}")
