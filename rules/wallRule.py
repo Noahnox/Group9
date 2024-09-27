e6246d582d4578dc5f754059a4b7fe766dc5ee02
Noahnox <noah.whiddoncarlsson@gmail.com>
2024-09-27 18:48:41 +0200

    Update wallRule.py

--- rules/wallRule.py
+++ rules/wallRule.py
@@ -1,20 +1,9 @@
 import ifcopenshell
-model = ifcopenshell.open('C:\Users\noahw\OneDrive\Skrivbord\model')
+#model = ifcopenshell.open('C:\Users\noahw\OneDrive\Skrivbord\model')
 
-walls = model.by_type('IfcWall')
-print(f"Walls in model: {len(walls)}")
-
-####Script#####
-import os
-
-# Clearing the Screen
-os.system('cls')
-
-import ifcopenshell
-from bonsai.bim.ifc import IfcStore
-file=IfcStore.get_file()
-wall = file.by_type('IfcWall')
+def num_walls_flors(model):
+  wall = model.by_type('Ifcwall') 
 print("Number of walls:",len(wall))
 
-floor = file.by_type('IfcBuildingStorey')
+floor = model.by_type('IfcBuildingstorey')
 print("Number of floors:",len(floor))
